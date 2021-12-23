ea5c8e7080ae621935be493a15ac0374d632e7e5
jefferson <rojefferson3@gmail.com>
2021-12-23 02:10:54 +0000

    merge voo

--- commons/entidade/voo.ts
+++ commons/entidade/voo.ts
@@ -3,4 +3,7 @@ export class Voo {
     horarioSaida!: Date;
     localPartida!: string;
     localDestino!: string;
-}
+    Duracao!: string;
+    Assentos!: string;
+    Empresa!: string;
+    constructor() { }}
